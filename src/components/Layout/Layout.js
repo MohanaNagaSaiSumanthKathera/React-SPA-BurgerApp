50887d847508846283023bdc63b1be00de2f1c14
Sumanth Kathera <Sumanth.Kathera@arm.com>
2019-11-09 10:31:32 +0000

    added logo and toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Auxi from '../../hoc/auxi';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 const Layout = (props)=>(
     <Auxi>
-            <div>Toolbar,sidedrawer,backdrop</div>
+            <Toolbar/>
+            <div>sidedrawer,backdrop</div>
             <main className={classes.Content}>{props.children}</main>
         </Auxi>
 )
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Logo.css';
+import BurgerLogo from '../../assets/images/burger-logo.png';
+
+const Logo = () =>{
+    return <div className={classes.Logo}> 
+                <img src={BurgerLogo} alt="MyBurger"/>
+            </div>
+}
+
+export default Logo;
