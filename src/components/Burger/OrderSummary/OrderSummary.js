ddf5aea1dff08136f5056fdf6849a12f0f03e1b8
Sumanth Kathera <Sumanth.Kathera@arm.com>
2019-11-09 13:09:57 +0000

    restructured the layout folder to hoc

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,25 +2,26 @@ import React from 'react';
 import Auxi from '../../../hoc/auxi';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary=(props)=>{
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends React.Component{    
+    
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
                 .map((igKey)=>{
-                    return <li key={igKey}><span style={{textTransform:'capitalize'}}>{igKey} : {props.ingredients[igKey]}</span></li>
+                    return <li key={igKey}><span style={{textTransform:'capitalize'}}>{igKey} : {this.props.ingredients[igKey]}</span></li>
                 })
-
-    return(
-        <Auxi>
+        return(
+            <Auxi>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients::</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price :: {props.price}</strong></p>
+            <p><strong>Total Price :: {this.props.price}</strong></p>
             <p>Continue to checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCancelled}>Cancel</Button>
-            <Button btnType='Success' clicked={props.purchaseContinue}>Continue</Button>
+            <Button btnType='Danger' clicked={this.props.purchaseCancelled}>Cancel</Button>
+            <Button btnType='Success' clicked={this.props.purchaseContinue}>Continue</Button>
         </Auxi>
-    )
+        );
+    }   
 }
-
 export default OrderSummary;
